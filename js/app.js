5c90917b4bd834c48497508fdde81b83784ad8e4
KaitoHH <hh.kaito@gmail.com>
2017-03-02 23:42:15 +0800

    feat: Player entity and Enemy entity

--- js/app.js
+++ js/app.js
@@ -1,32 +1,100 @@
-// 这是我们的玩家要躲避的敌人 
-var Enemy = function() {
-    // 要应用到每个敌人的实例的变量写在这里
-    // 我们已经提供了一个来帮助你实现更多
+var numRows = 6,
+    numCols = 5;
+var Entity = function(col, row, resource) {
+    this.x = 0;
+    this.y = 0;
+    this.col = col;
+    this.row = row;
+    this.sprite = resource;
+}
 
-    // 敌人的图片或者雪碧图，用一个我们提供的工具函数来轻松的加载文件
-    this.sprite = 'images/enemy-bug.png';
+Entity.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+Entity.prototype.updateXY = function() {
+    this.x = this.col * 101;
+    this.y = this.row * 83 - 20;
+}
+
+// 这是我们的玩家要躲避的敌人
+var Enemy = function(col, row, velocity) {
+    Entity.call(this, col, row, 'images/enemy-bug.png');
+    this.velocity = velocity;
+};
+
+Enemy.prototype = Object.create(Entity.prototype);
+Enemy.prototype.constructor = Enemy;
 // 此为游戏必须的函数，用来更新敌人的位置
 // 参数: dt ，表示时间间隙
 Enemy.prototype.update = function(dt) {
     // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
     // 都是以同样的速度运行的
-};
-
-// 此为游戏必须的函数，用来在屏幕上画出敌人，
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    this.col += this.velocity * dt;
+    if (this.col > numCols)
+        this.col = -1;
+    this.updateXY();
 };
 
 // 现在实现你自己的玩家类
 // 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
+var Player = function() {
+    // The initial position of player is at middle bottom of the ground.
+    Entity.call(this, 2, 5, 'images/char-boy.png');
+    this.score = 0;
+}
 
+Player.prototype = Object.create(Entity.prototype);
+Player.prototype.constructor = Player;
+
+//when player is hit by an enemy, this function will be invoked
+Player.prototype.resetPosition = function() {
+    player.col = 2;
+    player.row = 5;
+}
+Player.prototype.update = function() {
+    this.updateXY();
+}
+
+Player.prototype.handleInput = function(key) {
+    switch (key) {
+        case 'left':
+            if (this.col > 0)
+                this.col--;
+            break;
+        case 'up':
+            if (this.row > 0)
+                this.row--;
+            // player reached the top of the ground.
+            if (this.row == 0) {
+                this.score++;
+                // reset the position
+                this.row = numRows - 1;
+            }
+            break;
+        case 'right':
+            if (this.col < numCols - 1)
+                this.col++;
+            break;
+        case 'down':
+            if (this.row < numRows - 1)
+                this.row++;
+            break;
+    }
+}
 
 // 现在实例化你的所有对象
 // 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
 // 把玩家对象放进一个叫 player 的变量里面
-
+allEnemies = [
+    new Enemy(-1, 1, 2),
+    new Enemy(-1, 2, 2.3),
+    new Enemy(-1, 3, 3.9),
+    new Enemy(2, 2, 2.6),
+    new Enemy(3, 3, 1.3),
+    new Enemy(1, 1, 4.1)
+];
+var player = new Player();
 
 // 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Play.handleInput()
 // 方法里面。你不需要再更改这段代码了。
